d8a0c59829b6cce7a1555dfdd919ca46fee86c98
LigoliPuschkin <80857047+LigoliPuschkin@users.noreply.github.com>
2024-07-07 05:53:36 +0200

    Update nonetworkboot.sh

--- nonetworkboot.sh
+++ nonetworkboot.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 #
 net=/etc/init.d/networking
-
+NEt=/etc/runlevels/async
 echo "====================> faster boot"
 rc-update del networking boot
-mkdir /etc/runlevels/async
+mkdir $NEt
 rc-update add -s default async
 printf "::once:/sbin/openrc async" >> /etc/inittab
 rc-update add networking async
 
 if test -f "$net"; then
-  rm $net
+  if test -f "$NEt/networking"; then
+    rm $NEt/networking
+  fi
+  cp $net $NEt 
 fi
